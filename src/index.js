4fe440875f727c29a183b2862e91b70239dff3b0
Anil Bhadani <anil.bhadani99@gmail.com>
2022-10-07 18:22:16 +0200

    add shuffle cards and winning animation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import shuffle from "shuffle-array";
-
+import Confetti from "react-confetti";
+import useWindowSize from "react-use/lib/useWindowSize";
 import "./styles.css";
-
-import { run } from "./animation";
-
-function Animation() {
-  useEffect(() => {
-    run();
-  });
-  return <canvas id="canvas" />;
-}
 
 function Tile({ id, children, onToggle, isSet }) {
   return (
@@ -34,7 +26,6 @@ const bingoWords = [
   "Could you share this slides afterwards?",
   "Can somebody grant presenter rights?",
   "Can you email that to everyone?",
-  "CONF CALL 😷 BINGO",
   "Sorry i had problems logging in",
   "(animal noises in the background)",
   "Sorry, i didn't found the conference I'd",
@@ -49,41 +40,86 @@ const bingoWords = [
   "Can you repeat, please?",
 ];
 
+let bingoWordsShuffle = Object.values(shuffle(bingoWords).reduce((data, value, index) => ({ ...data, [index]: value }), {}));
+bingoWordsShuffle.splice(12, 0, "CONF CALL 😷 BINGO");
+
+const range = [0, 1, 2, 3, 4];
+let completeRow = [];
+let completeCol = [];
+let temp;
+
 function App() {
+  const { width, height } = useWindowSize();
   useEffect(() => {
     toggle(12);
   }, []);
 
   const [state, setState] = useState({ checked: {} });
-  const isWon = (checked) => {
-    const range = [0, 1, 2, 3, 4];
+  const [isRun, setIsRun] = useState(false);
 
-    return (
+  useEffect(() => {
+    setTimeout(() => {
+      setIsRun(false);
+    }, 3000);
+  }, [isRun]);
+
+  const isWon = (checked) => {
+    if (
       undefined !==
-        range.find((row) =>
+      range.find((row) => {
+        temp = row;
+        return (
+          !completeRow.includes(temp) &&
           range.every((column) => {
             return checked[row * 5 + column];
           })
-        ) ||
+        );
+      })
+    ) {
+      completeRow.push(temp);
+      return true;
+    }
+
+    if (
       undefined !==
-        range.find((column) =>
+      range.find((column) => {
+        temp = column;
+        return (
+          !completeCol.includes(temp) &&
           range.every((row) => {
             return checked[row * 5 + column];
           })
-        ) ||
+        );
+      })
+    ) {
+      completeCol.push(temp);
+      return true;
+    }
+
+    if (
       range.every((index) => {
         return checked[index * 5 + index];
-      }) ||
+      })
+    ) {
+      return true;
+    }
+
+    if (
       range.every((index) => {
         return checked[index * 5 + 4 - index];
       })
-    );
+    ) {
+      return true;
+    }
+    return false;
   };
   const toggle = (id) =>
     setState((state) => {
       const checked = { ...state.checked, [id]: !state.checked[id] };
-      const checked1 = { ...state.checked, [id]: !state.checked[id] };
-      const won = isWon(checked1);
+      const won = isWon(checked);
+      console.log("won : ", won);
+      setIsRun(won);
+
       return {
         ...state,
         checked,
@@ -95,13 +131,13 @@ function App() {
     <div className="App">
       <h1>Bingo</h1>
       <div className="wrapper">
-        {Object.keys(bingoWords).map((id) => (
+        {Object.keys(bingoWordsShuffle).map((id) => (
           <Tile key={id} id={id} isSet={!!state.checked[id]} onToggle={() => toggle(id)}>
-            {bingoWords[id]}
+            {bingoWordsShuffle[id]}
           </Tile>
         ))}
       </div>
-      {state.won ? <Animation /> : null}
+      {state.won && isRun && <Confetti width={width} height={height} />}
     </div>
   );
 }
